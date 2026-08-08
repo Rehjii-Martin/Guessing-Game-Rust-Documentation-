5f4a33d1c4f6d2d2328be8e32f97a629ff7776b8
Rehjii-Martin <rehjiimartin15@gmail.com>
2026-08-07 18:03:43 -0700

    Added matching logic and imported cmp::Ordering for matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use std::io;
 
 use rand::Rng;
@@ -18,4 +20,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp($secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too large!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
